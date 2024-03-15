bb7e63412f21caa1952e7ac35d18ae5ab6c4cbf7
Ruairidh MacArthur <roomacarthur@gmail.com>
2024-03-15 14:41:15 +0000

    updated DEV call in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get("dev"):
+if os.environ.get("DEV"):
     DEBUG = True
 else:
     DEBUG = False
