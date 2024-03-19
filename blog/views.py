61ff3004aa19e6b17160a1181938fda2e72c3874
Ruairidh MacArthur <roomacarthur@gmail.com>
2024-03-19 13:19:46 +0000

    Blog View & Templates added views, template and urls for the blog list

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostList(generic.ListView):
+    model = Post
+    template_name = "post_list.html"
 
-
-def my_blog(request):
-    return HttpResponse("Hello, Blog!")
