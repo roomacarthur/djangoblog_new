75d0f4cd9ac8ece7c0979b34e871df45b10905e2
Ruairidh MacArthur <roomacarthur@gmail.com>
2024-03-20 13:51:37 +0000

    URL Fix re organised urls to allow for index.html to be root urland removed url conflict

--- codestar/urls.py
+++ codestar/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("blog/", include("blog.urls")),  # Prefixed with blog/
     path("about/", include("about.urls")),  # Prefixed with about/
+    path("", include("blog.urls")),  # Prefixed with blog/
     path('summernote/', include('django_summernote.urls')),
 ]
